VoltageOS/hardware_voltage_interfaces

interfaces: cleanup
Change-Id: I108efa15757d0372f4fdd63a12aabc3cd34a8189
Signed-off-by: Dmitrii <bankersenator@gmail.com>

update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.voltage:hardware/voltage/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"

ApolloOS/hardware_apollo_interfaces

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.apollo:hardware/apollo/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"